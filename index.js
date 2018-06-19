0ddb9d5ae64c3d4255d2ac823c9a0bed44848d6e
nermolaev <tazaryoot09@gmail.com>
2018-06-19 15:01:37 +0300

    add common sraping script

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 /*jshint esversion: 6 */
+
+const needle = require('needle');
+const tress = require('tress');
+
+const url = 'http://t02.gazprom.dev.design.ru/';
+
+let results = [];
+
+let q = tress((url, callback) => {
+
+  needle.get(url, (err, res) => {
+
+    if (err) {
+      throw err;
+    }
+
+    callback();
+  });
+
+});
+
+q.drain = function() {
+  require('fs').writeFileSync('./result.json', JSON.stringify(results, null, 4));
+};
+
+q.push(url);
