fb06c460c6da460d134a3fb20d6329be62471cb3
nermolaev <tazaryoot09@gmail.com>
2019-02-22 17:23:27 +0300

    added config.dist.js

---
+++ config.dist.js
@@ -0,0 +1,14 @@
+const config = {
+  urlCore: 'http://t02.gazprom.dev.design.ru',
+  urlMap: '',
+  scrappingModulePath: './lib/scrap_modules/testAllSite',
+  excludeURL: /http|-de|-en|javascript:|\/f\/|mailto:|tel:|\/#/,
+  resultPath: './build/client/app/assets/',
+  exportSettings: {
+    fields: ['page', 'title', {label: 'tag', value: 'tags.name'}, {label: 'text', value: 'tags.list.text'}],
+    unwind: ['tags','tags.list'],
+    delimiter: ';',
+  }
+};
+
+module.exports = config;
